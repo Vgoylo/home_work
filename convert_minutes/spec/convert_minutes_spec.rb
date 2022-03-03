f1ae108cd27c628b6999deb99117702465e82b7b
vgoylo <y_draiv.by@mail.ru>
2022-03-03 13:08:09 +0300

    Refactoring

--- convert_minutes/spec/convert_minutes_spec.rb
+++ convert_minutes/spec/convert_minutes_spec.rb
@@ -2,10 +2,12 @@ require_relative '../lib/convert_minutes'
 require 'pry'
 
 RSpec.describe 'ConvertMinutes' do
-  context '#converter' do
-    let(:convert_minutes) { ConvertMinutes.new(5) }
-    it 'is valid' do
-      expect(convert_minutes.converter).to eq 300
+  describe '#converter' do
+    context 'is valid'
+      let(:convert_minutes) { ConvertMinutes.new(5) }
+      it 'is valid' do
+        expect(convert_minutes.converter).to eq 300
+      end
     end
   end
 end
