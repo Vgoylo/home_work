a54dc9e6418180155dbfca59335b7370b3f6e157
vgoylo <y_draiv.by@mail.ru>
2022-03-03 12:34:50 +0300

    Added check classes

--- sum_number/lib/sum_number.rb
+++ sum_number/lib/sum_number.rb
@@ -3,12 +3,22 @@
 class SumNumber
   attr_accessor :a, :b
 
-  def initialize(a, b)
-    @a = a
-    @b = b
+  def initialize(a = nil, b = nil)
+    @a, @b = a.to_f , b.to_f
   end
 
   def sum_of_numbers
-    a + b
+    a + b if scheck_clases?
+  end
+
+  private
+
+  def scheck_clases?
+    check_string_if_integer?(a) && check_string_if_integer?(b)
+  end
+
+  def check_string_if_integer?(inctanse)
+    a.to_i && b.to_i
+    inctanse.is_a?(Integer) || inctanse.is_a?(Float)
   end
 end
